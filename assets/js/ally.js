625da605e12d2c7367bbcd67e33a7417e30f2e7d
Brian Lovely <brian@studiobl.com>
2016-04-15 13:18:17 -0400

    Added files via upload

---
+++ assets/js/ally.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+	
+	$.getJSON( "code-test.json", function( data ) {
+		  var items = [];
+		  $.each( data, function( key, val ) {
+		    items.push( "<li id='" + key + "'>" + val + "</li>" );
+		  });
+		 
+		  $( "<ul/>", {
+		    "class": "my-new-list",
+		    html: items.join( "" )
+		  }).appendTo( "body" );
+		});
+});
