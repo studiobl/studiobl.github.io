7eded6d63c2e2e85402f6a3b467c654150783aeb
Brian Lovely <brian@studiobl.com>
2016-04-15 20:31:53 -0400

    Update ally.js

--- assets/js/ally.js
+++ assets/js/ally.js
@@ -1,14 +1,10 @@
 $( document ).ready(function() {
-	
-	$.getJSON( "https://studiobl.github.io/assets/js/code-test.json", function( data ) {
-		  var items = [];
-		  $.each( data, function( key, val ) {
-		    items.push( "<li id='" + key + "'>" + val + "</li>" );
-		  });
-		 
-		  $( "<ul/>", {
-		    "class": "my-new-list",
-		    html: items.join( "" )
-		  }).appendTo( "body" );
-		});
+	$.getJSON( "code-test.json", function( data ) {
+		
+		for(i=0; i<data.length; i++){
+			dataObj = data[i];
+			string = "<tr><td>" + dataObj.name + "</td><td>" + dataObj.apy + "%</td><td>$" + dataObj.earnings + "</td></tr>";
+			$("#rates_table tbody").append(string);
+		};
+	});
 });
